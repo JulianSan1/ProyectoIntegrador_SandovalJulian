d1c5b25b84534e4fa50aad93230ec7e6a1154449
julian sandoval <sandovaljulian679@gmail.com>
2022-09-09 05:53:57 -0300

    ñam

--- PortfolioWeb/src/app/app.module.ts
+++ PortfolioWeb/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoApComponent } from './component/logo-ap/logo-ap.component';
+import { SocialComponent } from './component/social/social.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent
+  
   ],
   imports: [
     BrowserModule,
