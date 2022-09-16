f1dfed5856db606b0c147b765f3c61a67ad693e1
julian sandoval <sandovaljulian679@gmail.com>
2022-09-16 02:17:43 -0300

    Agregar mock de tareas y el interfaz de las mismas

---
+++ talk-list/src/app/mosk.tasks.ts
@@ -0,0 +1,28 @@
+import {task} from "./task"
+export const TASKS = [
+
+    {
+        id: 1,
+        Text: "Terminar primer modulo de angular",
+        day: 'agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        Text: "Hacer las compras para la cena",
+        day: 'agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        Text: "Investigar sobre bootstrap",
+        day: 'agosto 5 a las 19:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        Text: "Lerr mi libro favorito",
+        day: 'agosto 5 a las 21:00',
+        reminder: false
+    }
+]
