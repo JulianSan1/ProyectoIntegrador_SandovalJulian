72ebdc5713ffb3a3911c6dde22e43a98b3763fda
julian sandoval <sandovaljulian679@gmail.com>
2022-09-16 02:58:34 -0300

    Lista de items

--- talk-list/src/app/mosk.tasks.ts
+++ talk-list/src/app/mosk.tasks.ts
@@ -1,27 +1,26 @@
-import {task} from "./task"
-export const TASKS = [
-
+import {Task} from "./Task"
+export const TASKS: Task[] = [
     {
         id: 1,
-        Text: "Terminar primer modulo de angular",
+        text: "Terminar primer modulo de angular",
         day: 'agosto 5 a las 12:00',
         reminder: true
     },
     {
         id: 2,
-        Text: "Hacer las compras para la cena",
+        text: "Hacer las compras para la cena",
         day: 'agosto 5 a las 17:00',
         reminder: true
     },
     {
         id: 3,
-        Text: "Investigar sobre bootstrap",
+        text: "Investigar sobre bootstrap",
         day: 'agosto 5 a las 19:00',
         reminder: false
     },
     {
         id: 4,
-        Text: "Lerr mi libro favorito",
+        text: "Leer mi libro favorito",
         day: 'agosto 5 a las 21:00',
         reminder: false
     }
